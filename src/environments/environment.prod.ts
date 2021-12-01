4acca2e3b6091056a258db673ece87c7ef3ad658
LancerJackson <gmmustang.jackson@gmail.com>
2021-12-01 17:39:07 -0300

    firebase em producao

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCpnshhg_l6UROVIjEVdI29ZB9hrRegEoE",
+    authDomain: "uam-shopp.firebaseapp.com",
+    databaseURL: "https://uam-shopp-default-rtdb.firebaseio.com",
+    projectId: "uam-shopp",
+    storageBucket: "uam-shopp.appspot.com",
+    messagingSenderId: "808391543595",
+    appId: "1:808391543595:web:fbc6f6f6416d5b55ed936e",
+  }
 };
